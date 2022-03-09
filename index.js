3cf71fe591c2645fbed2e2c21e3250c8067c7758
Rony <ronyramos495@gmail.com>
2022-03-08 20:13:38 -0500

    add hover and behavier with script

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const range = document.querySelector("input[type='range']");
+const costMonth = document.getElementById('month');
+const check = document.getElementById('check');
+
+
 
 range.addEventListener('input', (event) => {
     range.style.backgroundSize = range.value + "% 100%";
+    
+    if(check.checked) {
+        let value = (16 / 50) * (range.value);
+        let desc =  (value * 25) / 100;
+        let finalCost = value - desc;
+        costMonth.innerText = "$" + String((Math.round(finalCost*100)/100));
+    }
+
+    else {
+        let value = (16 / 50) * (range.value);
+        costMonth.innerText = "$" + String((Math.round(value*100)/100));
+    }
 });
-console.log(range);
+
+
+check.addEventListener( 'click', (event) => {
+    if(check.checked) {
+        let value = (16 / 50) * (range.value);
+        let desc =  (value * 25) / 100;
+        let finalCost = value - desc;
+        costMonth.innerText = "$" + String((Math.round(finalCost*100)/100));
+    }
+
+    else {
+        let value = (16 / 50) * (range.value);
+        costMonth.innerText = "$" + String((Math.round(value*100)/100));
+    }
+});
